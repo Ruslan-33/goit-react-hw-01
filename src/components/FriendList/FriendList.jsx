07f9de52783673f2cbe7996ae8b6a7c117317cae
Ruslan-33 <newmiknet@gmail.com>
2024-05-28 22:25:27 +0300

    28524

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,17 @@
+import css from './FriendList.module.css';
+
+export default FriendList({ friend });
+{
+  //   return (
+  <ul>
+    // {/* Кількість li залежить від кількості об'єктів в масиві */}
+    <li>
+      <FriendListItem />
+    </li>
+  </ul>;
+  //   );
+}
+
+// <>
+//   <FriendList friends={friends} />
+// </>
---
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -0,0 +1,10 @@
+import css from './FriendListItem.module.css'
+
+export default FriendListItem({avatar, name, isOnline}) {
+    <div>
+        <img src="" alt="Avatar" width="48" />
+        <p>Friend name</p>
+        <p>Friend status</p>
+    </div>
+
+}
