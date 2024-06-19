61726d361f345cddf4ec3e61606fbb102b6bbec6
Ruslan-33 <newmiknet@gmail.com>
2024-06-19 23:41:54 +0300

    second_task_finish

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -5,10 +5,10 @@ export default function FriendListItem({
   friends: { avatar, name, isOnline },
 }) {
   return (
-    <div className={css.friends}>
+    <div className={css.friend}>
       <img src={avatar} alt={name} width="48" />
       <p className={css.name}>{name}</p>
-      <p className={clsx(isOnline === true ? css.Offline : css.Online)}>
+      <p className={clsx(isOnline === true ? css.offline : css.online)}>
         {isOnline === true ? 'OnLine' : 'OffLine'}
       </p>
     </div>
